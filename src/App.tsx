aec40ccecb94bc3d2ef23019d71bc9a9e391a8b5
alberto <alberto85ariel13@gmail.com>
2022-12-16 16:31:33 -0500

    feat: add switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
-
+import SwitchGrade from './components/switch/switch';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <SwitchGrade defaultCelsius={true} changeCelsius ={(celsius: boolean) =>console.log(celsius)} ></SwitchGrade>
       </header>
+      <div className="Container">
+        
+      </div>
     </div>
   );
 }
